0cfd1aeedaca9a90e15161f4733509eb5a5dc036
Kyaa Goggins <kyaagoggins@gmail.com>
2025-03-24 11:50:40 -0700

    remove secrets

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ import axios from 'axios';
 
 const app = express();
 const PORT = 3000;
-const CLIENT_ID = 'J06btbshqZYna6oXE2Kn8LbU8xPRemIZ4mSYCOThUuoT8tVq0Z';
-const CLIENT_SECRET = 'qyuM8A8wbCCR90igrH2UA2NH0fohnfAMh96IEg0g';
+
 
 app.use(cors());
 app.use(express.json());
@@ -17,14 +16,6 @@ app.get('/api/pets', async (request, result) => {
             return result.status(400).json({ error: 'Missing PetFinder ID' });
         }
 
-        //get token 
-        const tokenResponse = await axios.post('https://api.petfinder.com/v2/oauth2/token', {
-            grant_type: 'client_credentials',
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET
-        });
-
-        const accessToken = tokenResponse.data.access_token;
 
         //fetch list of pets 
         const petResponse = await axios.get(`https://api.petfinder.com/v2/animals?organization=${urlPetId}`, {
